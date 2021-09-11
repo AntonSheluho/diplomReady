b39e0b5ffeece50c38c29760757a8e647415751c
AntonSheluho <grinfrend2@gmail.com>
2021-09-11 16:02:06 +0300

    less

--- adres.js
+++ adres.js
@@ -1,7 +1,14 @@
 let burger = document.querySelector('.content__burger');
 let menu = document.querySelector('.content__menu');
+let loke = document.querySelector('body')
 
 burger.onclick = function reade() {
     menu.classList.toggle('active');
     burger.classList.toggle('active');
+    //loke.classList.toggle('loke');
 };
+
+loke.onscroll = function back() {
+    menu.classList.remove('active');
+    burger.classList.remove('active');
+}
